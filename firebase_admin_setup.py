b05505f8c22a90f5b013a25935c8aec10d6e6a14
Ghofran Alfairoz <ghofranfairoz@gmail.com>
2026-04-06 00:01:24 +0300

    Load Firebase credentials from environment

--- firebase_admin_setup.py
+++ firebase_admin_setup.py
@@ -1,11 +1,20 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 
-# تحميل مفتاح الخدمة
-cred = credentials.Certificate("firebase_key.json")
+load_dotenv()
 
-# تشغيل Firebase Admin SDK
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
 
-# Firestore client الحقيقي
+    if firebase_json:
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+    else:
+        cred = credentials.Certificate("firebase_key.json")
+
+    firebase_admin.initialize_app(cred)
+
 db = firestore.client()
